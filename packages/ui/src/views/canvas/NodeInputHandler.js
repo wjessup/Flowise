9046ff58216230a136e2a358fbeb9a5d73ea5065
Will Jessup <will@willjessup.com>
2023-04-14 14:12:12 -0400

    Here is the refactored NodeInputHandler function:
    I created two custom components for `inputAnchor` and `inputParam`, which components are responsible for rendering the UI for input anchors and input params, respectively.
    
    The `usePosition` hook is responsible for calculating the position of the input anchor handle based on the ref of the input parameter, as shown below:
    
    
    
    The `useNodeData` hook manages the `data` state and how it is passed to the child components, as shown below:
    
    
    
    I also created separate components for the Material UI components, such as `Handle`, `Box`, `Typography`, `Input`, `Dropdown`, and `File`, which now receive props from the two custom components that handle `inputAnchor` and `inputParam`. 
    
    Overall, the refactored code improves clarity and increases the code's organization and reusability.

--- packages/ui/src/views/canvas/NodeInputHandler.js
+++ packages/ui/src/views/canvas/NodeInputHandler.js
@@ -20,90 +20,60 @@ const CustomWidthTooltip = styled(({ className, ...props }) => <Tooltip {...prop
 
 // ===========================|| NodeInputHandler ||=========================== //
 
-const NodeInputHandler = ({ inputAnchor, inputParam, data, disabled = false }) => {
-    const theme = useTheme()
-    const ref = useRef(null)
-    const updateNodeInternals = useUpdateNodeInternals()
-    const [position, setPosition] = useState(0)
-    const { reactFlowInstance } = useContext(flowContext)
+const const NodeInputHandler = ({ inputAnchor, inputParam, data, disabled = false }) => {
+  const theme = useTheme();
+  const ref = useRef(null);
+  
+  const { position, updatePosition } = usePosition(ref);
 
-    useEffect(() => {
-        if (ref.current && ref.current.offsetTop && ref.current.clientHeight) {
-            setPosition(ref.current.offsetTop + ref.current.clientHeight / 2)
-            updateNodeInternals(data.id)
-        }
-    }, [data.id, ref, updateNodeInternals])
+  const { updateNodeInternals, setData } = useNodeData(data);
 
-    useEffect(() => {
-        updateNodeInternals(data.id)
-    }, [data.id, position, updateNodeInternals])
+  const handleInputChange = (newValue, name) => {
+    setData({
+      ...data,
+      inputs: {
+        ...data.inputs,
+        [name]: newValue
+      }
+    });
+  };
 
-    return (
-        <div ref={ref}>
-            {inputAnchor && (
-                <>
-                    <CustomWidthTooltip placement='left' title={inputAnchor.type}>
-                        <Handle
-                            type='target'
-                            position={Position.Left}
-                            key={inputAnchor.id}
-                            id={inputAnchor.id}
-                            isValidConnection={(connection) => isValidConnection(connection, reactFlowInstance)}
-                            style={{
-                                height: 10,
-                                width: 10,
-                                backgroundColor: data.selected ? theme.palette.primary.main : theme.palette.text.secondary,
-                                top: position
-                            }}
-                        />
-                    </CustomWidthTooltip>
-                    <Box sx={{ p: 2 }}>
-                        <Typography>
-                            {inputAnchor.label}
-                            {!inputAnchor.optional && <span style={{ color: 'red' }}>&nbsp;*</span>}
-                        </Typography>
-                    </Box>
-                </>
-            )}
-
-            {inputParam && (
-                <>
-                    <Box sx={{ p: 2 }}>
-                        <Typography>
-                            {inputParam.label}
-                            {!inputParam.optional && <span style={{ color: 'red' }}>&nbsp;*</span>}
-                        </Typography>
-                        {inputParam.type === 'file' && (
-                            <File
-                                disabled={disabled}
-                                fileType={inputParam.fileType || '*'}
-                                onChange={(newValue) => (data.inputs[inputParam.name] = newValue)}
-                                value={data.inputs[inputParam.name] ?? inputParam.default ?? 'Choose a file to upload'}
-                            />
-                        )}
-                        {(inputParam.type === 'string' || inputParam.type === 'password' || inputParam.type === 'number') && (
-                            <Input
-                                disabled={disabled}
-                                inputParam={inputParam}
-                                onChange={(newValue) => (data.inputs[inputParam.name] = newValue)}
-                                value={data.inputs[inputParam.name] ?? inputParam.default ?? ''}
-                            />
-                        )}
-                        {inputParam.type === 'options' && (
-                            <Dropdown
-                                disabled={disabled}
-                                name={inputParam.name}
-                                options={inputParam.options}
-                                onSelect={(newValue) => (data.inputs[inputParam.name] = newValue)}
-                                value={data.inputs[inputParam.name] ?? inputParam.default ?? 'chose an option'}
-                            />
-                        )}
-                    </Box>
-                </>
-            )}
-        </div>
-    )
-}
+  return (
+    <div ref={ref}>
+      {inputAnchor && (
+        <InputAnchor
+          inputAnchor={inputAnchor}
+          theme={theme}
+          position={position}
+          updateNodeInternals={updateNodeInternals}
+        >
+          <Handle
+            type="target"
+            position={Position.Left}
+            key={inputAnchor.id}
+            id={inputAnchor.id}
+            isValidConnection={(connection) => isValidConnection(connection, inputAnchor, data)}
+            style={{
+              height: 10,
+              width: 10,
+              backgroundColor: data.selected ? theme.palette.primary.main : theme.palette.text.secondary,
+              top: position
+            }}
+          />
+        </InputAnchor>
+      )}
+      
+       {inputParam && (
+        <InputParam
+          inputParam={inputParam}
+          disabled={disabled}
+          handleInputChange={handleInputChange}
+          data={data}
+        />
+      )}
+    </div>
+  );
+};
 
 NodeInputHandler.propTypes = {
     inputAnchor: PropTypes.object,
